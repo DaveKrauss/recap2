d02c203110a5bfa73e543433499cad22333213d1
David Krauss <davey.krauss@googlemail.com>
2023-08-10 11:52:21 +0200

    link js and add bookmark toggle

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // phew… not a lot going on here. Please add some code!
+
+const bookmark = document.querySelector('[data-js="bookmark"]');
+
+bookmark.addEventListener("click", () => {
+  bookmark.classList.toggle("bookmark--active");
+});
